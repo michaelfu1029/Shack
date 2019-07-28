14439c04c4f487fe87e01b19fd34a0ae8a035bcc
michaelfu1029 <michaelfu1029@gmail.com>
2019-07-28 12:33:28 -0700

    Converted to class based components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-function Form(props) {
-    return (
-        <form>
-            <label>
-                {props.title}:
-                <input type="text" name="name" placeholder="Choice"/>
-                <input type="number" min={0} name="value" placeholder="value"/>
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
-    )
+class Form extends React.Component{
+    render() {
+        return (
+            <form >
+                <label>
+                    {this.props.title}:
+                    <input type="text" name="name" placeholder="Choice"/>
+                    <input type="number" min={0} name="value" placeholder="value"/>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
+    }
 }
 export default Form;
