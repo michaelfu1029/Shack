ba4ff979dc23de250bbc870a2c28b586ba564113
michaelfu1029 <michaelfu1029@gmail.com>
2019-07-30 23:41:58 -0700

    integrated form to have 3 inputs that change the size of the pie chart accordingly

--- src/App.js
+++ src/App.js
@@ -12,7 +12,18 @@ class App extends React.Component{
       val2: 10,
       val3: 10
     }
+    this.handleChange = this.handleChange.bind(this)
   }
+
+
+  handleChange(v1, v2, v3) {
+    this.setState({
+      val1: v1,
+      val2: v2,
+      val3: v3
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,7 +31,7 @@ class App extends React.Component{
           <p className="Compony-name">Shack</p>
           <img src={logo} className="App-logo" alt="logo" />
           <Pie val1= {this.state.val1} val2= {this.state.val2} val3= {this.state.val3}/>
-          <Form title="Name"/>
+          <Form title="Name" handleChange={this.handleChange}/>
         </header>
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,23 @@ import React from "react";
 class Form extends React.Component{
     render() {
         return (
-            <form >
+            <form onSubmit={(event)=>{
+                event.preventDefault();
+                this.props.handleChange(
+                    parseInt(event.target.percent1.value, 10),
+                    parseInt(event.target.percent2.value, 10),
+                    parseInt(event.target.percent3.value, 10)
+                );
+                
+            }}>
                 <label>
                     {this.props.title}:
-                    <input type="text" name="name" placeholder="Choice"/>
-                    <input type="number" min={0} name="value" placeholder="value"/>
+                    {/* <input type="text" name="name" placeholder="Choice"/> */}
+                    <input type="number" min={0} name="percent1" placeholder="value"/>
+                    <input type="number" min={0} name="percent2" placeholder="value"/>
+                    <input type="number" min={0} name="percent3" placeholder="value"/>
                 </label>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit"/>
             </form>
         )
     }
